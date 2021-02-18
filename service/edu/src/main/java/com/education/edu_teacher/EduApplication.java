7d8e77395cd28b3d4034f5e0f54f66f8bd6ad173
zuel-hzy <1059144380@qq.com>
2021-02-19 00:57:59 +0800

    2.18.2 nacos依赖（springboot版本降级至2.2.1）

--- service/edu/src/main/java/com/education/edu_teacher/EduApplication.java
+++ service/edu/src/main/java/com/education/edu_teacher/EduApplication.java
@@ -2,16 +2,22 @@ package com.education.edu_teacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @ComponentScan(basePackages ={"com.education"})
 public class EduApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EduApplication.class, args);
+        try {
+            SpringApplication.run(EduApplication.class, args);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
